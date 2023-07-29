3e7bbd1b0cade4817481dff74a063e96ed38398c
devon-javier <140371446+devon-javier@users.noreply.github.com>
2023-07-29 21:37:08 +0800

    Stores trees of data

--- Graph.java
+++ Graph.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Graph {
@@ -50,6 +51,55 @@ public class Graph {
         }
 
         return friends;
+    }
+
+    public ArrayList<Integer> breadthFirstSearch(int root, int friend)  {
+
+        boolean[] visited = new boolean[num_acc];   // already initialized to false 
+        
+        LinkedList<Integer> queue = new LinkedList<>();
+        int currentAccount;
+
+        int[] previousAccounts = new int[num_acc];
+
+        visited[root] = true;
+        queue.add(root);
+        previousAccounts[root] = -1;           // -1 to signify root node
+
+
+        while(queue.isEmpty() == false) {
+            currentAccount = queue.poll();
+
+            if(currentAccount == friend)    {
+            return getPath(previousAccounts, friend);
+
+            } else {
+                for(int i = 0; i < friendships[currentAccount].length; i++) {
+                    if(this.checkConnection(currentAccount, i) && !visited[i] ) {
+                        queue.add(i);
+                        visited[i] = true;
+                        previousAccounts[i] = currentAccount;
+                    }
+                }
+            }
+        }
+
+        ArrayList<Integer> empty = new ArrayList<Integer>();
+        return empty;                       // if friend cannot become the current account
+
+    }
+
+    public ArrayList<Integer> getPath(int[] previousAccounts, int friend) {
+        ArrayList<Integer> path = new ArrayList<Integer>();
+        int currentAccount = friend;            // Work backwards
+        int start = 0;                          // Reverse the path to go from root to friend
+
+        while(currentAccount != -1) {           // -1 to signify root node
+            path.add(start, currentAccount);
+            currentAccount = previousAccounts[currentAccount];
+        }
+
+        return path;
     }
 
     public void addFriendship(int user1, int user2) {
